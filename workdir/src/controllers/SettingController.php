57b6c3e18ff82891817d88523d6179af1c91ac66
Firesnake23 <al@elementary-snakes.ch>
2022-11-10 19:01:38 +0100

    Add email to user
    #3

--- workdir/src/controllers/SettingController.php
+++ workdir/src/controllers/SettingController.php
@@ -4,6 +4,7 @@ namespace firesnake\isItRunning\controllers;
 
 use firesnake\isItRunning\entities\User;
 use firesnake\isItRunning\events\RequestEvent;
+use firesnake\isItRunning\http\RedirectResponse;
 use firesnake\isItRunning\http\Response;
 use firesnake\isItRunning\http\TextResponse;
 use firesnake\isItRunning\http\TwigResponse;
@@ -14,8 +15,30 @@ class SettingController
     public function overview(RequestEvent $requestEvent): Response
     {
         return new TwigResponse('settings/overview.html.twig', [
-            'authenticatedUser' => $this->getAuthenticatedUser($requestEvent)
+            'authenticatedUser' => $this->getAuthenticatedUser($requestEvent),
+            'mail' => $this->getAuthenticatedUser($requestEvent)->getMail()
         ]);
+    }
+
+    public function changeMail(RequestEvent $event): Response {
+        $authenticatedUser = $this->getAuthenticatedUser($event);
+
+        if($authenticatedUser == null) {
+            return new RedirectResponse('/');
+        }
+
+        if(isset($event->getRequest()->getPost()['mail'])) {
+            $mail = $event->getRequest()->getPost()['mail'];
+
+            /** @var IsItRunning $isItRunning */
+            $isItRunning = $event->getParam('isItRunning');
+            $userManager = $isItRunning->getUserManager();
+
+            $authenticatedUser->setMail($mail);
+            $userManager->saveUser($authenticatedUser);
+        }
+
+        return new RedirectResponse('/settings');
     }
 
     private function getAuthenticatedUser(RequestEvent $event) :?User
