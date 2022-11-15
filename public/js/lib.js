f40147bc53e99ee3a168bab7903c76e9a7a2b199
Firesnake23 <al@elementary-snakes.ch>
2022-11-15 18:33:20 +0100

    Add fillDataTarget method

--- public/js/lib.js
+++ public/js/lib.js
@@ -8,3 +8,18 @@ async function apiCall(url, data) {
         redirect: 'manual',
     });
 }
+
+function fillDataTarget(element) {
+    let sourceId = element.getAttribute('data-source').replace('#', '');
+    let targetId = element.getAttribute('data-target').replace('#', '');
+    
+    console.log(sourceId);
+    console.log(targetId);
+
+    let sourceHtml = document.getElementById(sourceId).innerHTML;
+    let targetElement = document.getElementById(targetId);
+
+    targetElement.className = targetElement.className.replace(' d-none', '');
+
+    targetElement.innerHTML = sourceHtml;
+}
